6401667c6730120c3154c5729805810a34d2c885
Ashu Chan <ashu.sakamoto@gmail.com>
2024-05-16 18:06:43 +0800

    refactor: update deploy scripts

--- scripts/Deploy.s.sol
+++ scripts/Deploy.s.sol
@@ -3,57 +3,64 @@
 pragma solidity 0.8.25;
 
 import "forge-std/Script.sol";
-import "contracts/core/MestSharesFactoryV1.sol";
-import "contracts/core/aggregator/AaveYieldAggregator.sol";
-import { MestERC1155 } from "contracts/core/MestERC1155.sol";
+import { SharesFactoryV1 } from "contracts/core/SharesFactoryV1.sol";
+import { SharesERC1155 } from "contracts/core/SharesERC1155.sol";
+import { AaveYieldAggregator } from "contracts/core/aggregator/AaveYieldAggregator.sol";
 import { BlankYieldAggregator } from "contracts/core/aggregator/BlankYieldAggregator.sol";
 import { IYieldAggregator } from "contracts/interface/IYieldAggregator.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { IAavePool } from "contracts/interface/IAave.sol";
 
 contract DeployScript is Script {
-    MestSharesFactoryV1 public sharesFactory;
-    MestERC1155 public sharesNFT;
+    SharesFactoryV1 public sharesFactory;
+    SharesERC1155 public sharesNFT;
 
     AaveYieldAggregator public aaveYieldAggregator;
     BlankYieldAggregator public blankYieldAggregator;
 
     // Optimism Sepolia
-    address public owner = 0xdA1d0C7f174effBA98Ea1E31424418DC9aeaEa22;
-    address public weth = 0x4200000000000000000000000000000000000006;
-    address public aavePool = 0xb50201558B00496A145fE76f7424749556E326D8;
-    address public aaveGateway = 0x589750BA8aF186cE5B55391B0b7148cAD43a1619;
+    address public OWNER = 0xdA1d0C7f174effBA98Ea1E31424418DC9aeaEa22;
+    address public WETH = 0x4200000000000000000000000000000000000006;
+    address public AAVE_POOL = 0xb50201558B00496A145fE76f7424749556E326D8;
+    address public AAVE_WETH_GATEWAY = 0x589750BA8aF186cE5B55391B0b7148cAD43a1619;
 
     // Optimism Mainnet
-    // address public owner = 0xdA1d0C7f174effBA98Ea1E31424418DC9aeaEa22;
-    // address public weth = 0x4200000000000000000000000000000000000006;
-    // address public aavePool = 0x794a61358D6845594F94dc1DB02A252b5b4814aD;
-    // address public aaveGateway = 0xe9E52021f4e11DEAD8661812A0A6c8627abA2a54;
+    // address public OWNER = 0xdA1d0C7f174effBA98Ea1E31424418DC9aeaEa22;
+    // address public WETH = 0x4200000000000000000000000000000000000006;
+    // address public AAVE_POOL = 0x794a61358D6845594F94dc1DB02A252b5b4814aD;
+    // address public AAVE_WETH_GATEWAY = 0xe9E52021f4e11DEAD8661812A0A6c8627abA2a54;
 
-    IERC20 public aWETH = IERC20(IAavePool(aavePool).getReserveData(weth).aTokenAddress);
+    IERC20 public aWETH = IERC20(IAavePool(AAVE_POOL).getReserveData(WETH).aTokenAddress);
 
-    string public baseURI = "https://mest.io/shares/";
+    string public constant BASE_URI = "https://v4v.com/shares/uri/";
+    uint256 public constant BASE_PRICE = 5000000000000000; // 0.005 ETH as base price
+    uint256 public constant INFLECTION_POINT = 1500;
+    uint256 public constant INFLECTION_PRICE = 102500000000000000;
+    uint256 public constant LINEAR_PRICE_SLOPE = 0;
 
     function run() public virtual {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        sharesNFT = new MestERC1155(baseURI);
+        sharesNFT = new SharesERC1155(BASE_URI);
 
-        sharesFactory = new MestSharesFactoryV1(
-            address(sharesNFT),
-            5000000000000000, // basePrice,
-            1500, // linearPriceSlope,
-            102500000000000000, // inflectionPoint,
-            0 // inflectionPrice
+        sharesFactory = new SharesFactoryV1(
+            address(sharesNFT), 
+            BASE_PRICE, 
+            INFLECTION_POINT, 
+            INFLECTION_PRICE, 
+            LINEAR_PRICE_SLOPE
         );
 
-        aaveYieldAggregator = new AaveYieldAggregator(address(sharesFactory), weth, aavePool, aaveGateway);
-        blankYieldAggregator = new BlankYieldAggregator(address(sharesFactory), weth);
+        aaveYieldAggregator = new AaveYieldAggregator(address(sharesFactory), WETH, AAVE_POOL, AAVE_WETH_GATEWAY);
+        blankYieldAggregator = new BlankYieldAggregator(address(sharesFactory), WETH);
 
         sharesNFT.setFactory(address(sharesFactory));
-        sharesNFT.transferOwnership(owner);
-        sharesFactory.transferOwnership(owner);
-        aaveYieldAggregator.transferOwnership(owner);
+        sharesFactory.migrate(address(aaveYieldAggregator));
+
+        sharesNFT.transferOwnership(OWNER);
+        sharesFactory.transferOwnership(OWNER);
+        aaveYieldAggregator.transferOwnership(OWNER);
 
         vm.stopBroadcast();
     }
